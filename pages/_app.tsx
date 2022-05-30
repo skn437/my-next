039701a995b374b00171033b4ae56fcd6c7c914f
SKN <skn437physx@gmail.com>
2022-05-30 18:55:09 +0600

    Holy Hell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { indigo } from "@mui/material/colors";
+
+const darkTheme = createTheme({
+	palette: {
+		primary: indigo
+	},
+	typography: {
+		fontFamily: "Quicksand"
+	}
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<RecoilRoot>
-			<Component {...pageProps}></Component>
-		</RecoilRoot>
+		<ThemeProvider theme={darkTheme}>
+			<RecoilRoot>
+				<Component {...pageProps}></Component>
+			</RecoilRoot>
+		</ThemeProvider>
 	);
 }
 
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,27 +1,80 @@
 import type { NextPage } from "next";
-import { Typography, Button, Container, } from "@mui/material";
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import styles from "@/pages/create.module.scss";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
 
 const Create: NextPage = () => {
+	const [title, setTitle] = useState("");
+	const [details, setDetails] = useState("");
+	const [titleError, setTitleError] = useState(false);
+	const [detailsError, setDetailsError] = useState(false);
+
 	return (
 		<Container>
 			<Typography 
 				variant="h6" 
 				component="h2" 
 				color="secondary"
-				gutterBottom
+				id={styles.heading}
 			>
 				Create New Note
 			</Typography>
 
-			<Button 
-			type="submit"
-			color="primary"
-			variant="contained"
-			onClick={() => console.log(`You clicked`)}
+			<form
+				noValidate
+				autoComplete="off"
+				onSubmit={e => {
+					e.preventDefault();
+					setTitleError(false);
+					setDetailsError(false);
+
+					if (title === "") {
+						setTitleError(true);
+					}
+					if (details === "") {
+						setDetailsError(true);
+					} 
+					if (title && details) {
+						console.log(`title: ${title} details: ${details}`);
+					}
+				}}
 			>
-				Submit
-			</Button>
+				<TextField
+					label="Note Title"
+					variant="outlined"
+					required
+					fullWidth
+					margin="dense"
+					error = {titleError}
+					onChange={e => setTitle(e.target.value)}
+				></TextField>
+				
+				<TextField
+					label="Note Details"
+					variant="outlined"
+					required
+					fullWidth
+					margin="dense"
+					multiline
+					minRows={4}
+					error={detailsError}
+					onChange={e => setDetails(e.target.value)}
+				></TextField>
+
+				<Button 
+					type="submit"
+					color="primary"
+					variant="contained"
+					endIcon={<KeyboardArrowRightIcon></KeyboardArrowRightIcon>}
+					id={styles.button}
+				>
+					Submit
+				</Button>
+			</form>
 		</Container>
 	);
 };
